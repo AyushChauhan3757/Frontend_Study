07527c0f92a553ce17ac4a94161d9d9443f072f1
Ayush Chauhan <65812075+AyushChauhan3757@users.noreply.github.com>
2024-07-19 11:27:57 +0000

    Advanced JS

---
+++ study/JS_21/script.js
@@ -0,0 +1,15 @@
+// IIFE (Immediately Invoked Function Expression)
+(function() {
+    console.log('Hello World');
+})();
+
+// Destructuring
+const { name, age } = { name: 'John', age: 30 };
+
+// Spread Operator
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5];
+
+// Hoisting
+console.log(x); // undefined
+var x = 10;
